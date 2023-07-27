f6fd96ff40455f2d3941d56c80303347eba33955
Ashok Chauhan <ashokbc@teamlocus.net>
2023-07-27 10:56:18 +0530

    -version 26 update info

--- templates/settings/admin-form.php
+++ templates/settings/admin-form.php
@@ -68,7 +68,7 @@ if(empty($_['aaochat_lead_country'])) {
          <h2>
             <?php p($l->t('Aao Business Chat Settings')); ?>
         </h2>
-        <span><?php p($l->t('Need Technical support? Contact us at support@aaochat.com')); ?></span>
+        <span><?php p($l->t('Need Technical support? Contact us at ')); ?><a href="mailto:<?php p($l->t('support@aaochat.com')); ?>" alt=""><u><?php p($l->t('support@aaochat.com')); ?></u></a></span>
     </div>
 
 <div id="aaochat-lead">
